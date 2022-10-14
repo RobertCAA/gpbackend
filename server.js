a88873c3166b89c18f206b6e1b74f16fee3d45ac
RobertCAA <rober.ce.az@gmail.com>
2022-10-14 18:05:18 +0200

    updated server.js coment

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 8000;
 
+// Middleware
+// Receives and parse JSON data
+app.use(express.json());
+
 // Loads static files from the public folder
 app.use("/", express.static(path.join(__dirname, "public")));
 
+// Router middleware
 app.use("/", require("./routes/root"));
 
 // Catch every other petition
